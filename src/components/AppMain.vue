8518f4e189cc35c889cd901c8b828ce938f2859f
FabioVasi <vasifabio96@gmail.com>
2023-09-28 14:36:50 +0200

    add searchMovie

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,13 +3,19 @@ import {state} from '../state.js';
 
 export default {
     name: 'AppMain',
+    emits: ['performSearch'],
     data() {
         return {
             state
         }
     },
     methods: {
-
+        searchMovie() {
+            console.log(this.state.searchText);
+            const url = this.state.base_url + `&query=${this.state.searchText}`;
+            console.log(url);
+            this.state.fetchMovies(url)
+        }
     },
     created() {
         state.fetchMovies(state.base_url);
@@ -18,6 +24,13 @@ export default {
 </script>
 
 <template>
+
+    <div class="container">
+        <div class="searchbox d-flex">
+            <input class="form-control" type="search" v-model="state.searchText" placeholder="Search a movie">
+            <button class="border-0 bg-primary text-white" @click="$emit('performSearch')">Search</button>
+        </div>
+    </div>
 
 </template>
 
--- src/state.js
+++ src/state.js
@@ -4,6 +4,8 @@ import { reactive } from "vue";
 export const state = reactive({
     base_url: 'https://api.themoviedb.org/3/search/movie?api_key=daba673b73b762af2494496b68204faf&query=Star',
     movies: null,
+    searchText: '',
+    movieTitle: '',
 
     fetchMovies(url) {
         console.log(this, this.base_url);
