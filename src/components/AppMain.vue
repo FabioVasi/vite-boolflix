7697ab1642a69d6372688be0b252f86307f2b957
FabioVasi <vasifabio96@gmail.com>
2023-09-28 15:19:44 +0200

    add cards

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -26,9 +26,20 @@ export default {
 <template>
 
     <div class="container">
-        <div class="searchbox d-flex">
+        <div class="searchbox d-flex mb-5">
             <input class="form-control" type="search" v-model="state.searchText" placeholder="Search a movie">
             <button class="border-0 bg-primary text-white" @click="$emit('performSearch')">Search</button>
+        </div>
+
+        <div class="row row-cols-1 row-cols-md-5 g-3">
+            <div class="col" v-for="movie in state.movies">
+                <div class="card h-100">
+                    <h2>Titolo: {{movie.title}}</h2>
+                    <h3>Titolo Originale: {{movie.original_title}}</h3>
+                    <p>Lingua: {{movie.original_language}}</p>
+                    <span>Voto: {{movie.vote_average}}</span>
+                </div>
+            </div>
         </div>
     </div>
 
--- src/state.js
+++ src/state.js
@@ -5,15 +5,14 @@ export const state = reactive({
     base_url: 'https://api.themoviedb.org/3/search/movie?api_key=daba673b73b762af2494496b68204faf&query=Star',
     movies: null,
     searchText: '',
-    movieTitle: '',
 
     fetchMovies(url) {
         console.log(this, this.base_url);
         axios
         .get(url)
         .then(response => {
-            console.log(response);
-            this.movies = response.data;
+            console.log(response.data.results);
+            this.movies = response.data.results;
         })
         .catch(error => {
             console.error(error);
