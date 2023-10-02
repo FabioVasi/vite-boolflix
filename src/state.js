6b18ece1de7b734abdc63eaa7da3119008114d2b
FabioVasi <vasifabio96@gmail.com>
2023-10-02 17:41:28 +0200

    end ex

--- src/state.js
+++ src/state.js
@@ -8,29 +8,31 @@ export const state = reactive({
     seriesTv: null,
     searchText: '',
     flags_url: 'https://flagsapi.com/',
-    castMovies: 'https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=daba673b73b762af2494496b68204faf',
-    castSeries: 'https://api.themoviedb.org/3/tv/{series_id}/credits?api_key=daba673b73b762af2494496b68204faf',
+    castMovies: 'https://api.themoviedb.org/3/movie/{movie.id}/credits?api_key=daba673b73b762af2494496b68204faf',
+    castSeries: 'https://api.themoviedb.org/3/tv/{series.id}/credits?api_key=daba673b73b762af2494496b68204faf',
+    movieCredits: '',
+    seriesCredits: '',
 
     fetchMovies() {
-        console.log(this, this.base_url, this.tvSeries_url);
+        console.log(this, this.base_url, this.tvSeries_url, this.castMovies, this.castSeries);
         axios
-        .get(this.base_url + `&query=${this.searchText}`)
-        .then(response => {
-            this.movies = response.data.results;
-            console.log(this.movies, this.castMovies);
-        })
-        .catch(error => {
-            console.error(error);
-        })
+            .get(this.base_url + `&query=${this.searchText}`)
+            .then(response => {
+                this.movies = response.data.results;
+                console.log(this.movies, this.castMovies);
+            })
+            .catch(error => {
+                console.error(error);
+            })
 
         axios
-        .get(this.tvSeries_url + `&query=${this.searchText}`)
-        .then(response => {
-            this.seriesTv = response.data.results;
-            console.log(this.seriesTv);
-        })
-        .catch(error => {
-            console.error(error);
-        })
+            .get(this.tvSeries_url + `&query=${this.searchText}`)
+            .then(response => {
+                this.seriesTv = response.data.results;
+                console.log(this.seriesTv);
+            })
+            .catch(error => {
+                console.error(error);
+            })
     }
 })
