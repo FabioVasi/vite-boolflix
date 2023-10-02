8124e68f762c70e782c70266c6e251a15ddbd4aa
FabioVasi <vasifabio96@gmail.com>
2023-10-02 11:19:35 +0200

    add title separation

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -22,10 +22,19 @@ export default {
     <section>
 
         <div class="container-fluid">
-        
+            <h2 class="text-white fs-1 text-center mt-5 mb-5">Movies</h2>
             <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 g-4">
-
+                
                 <CardMovie :movie="movie" v-for="movie in state.movies"/>
+
+            </div>
+
+        </div>
+
+        <div class="container-fluid">
+            <h2 class="text-white fs-1 text-center mt-5 mb-5">Series</h2>
+            <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 g-4">
+                
                 <CardSeries :series="series" v-for="series in state.seriesTv"/>
 
             </div>
